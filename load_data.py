7c48337bc8d6b3a87d64c29a9a6f381d48c8092b
flevinton <104422038+felicielevinton@users.noreply.github.com>
2025-06-16 11:33:46 +0200

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -43,7 +43,7 @@ if len(features_aone) >= len(features_pmc):
 else:
     features = features_pmc
 
-# pour récupérer les psth alignés sur les changements de fréquence : 
+# pour récupérer les psth alignés sur les changements de fréquence (uniquement les good glusters): 
 
 tracking_pmc = get_psth(data_pmc, features, t_pre, t_post, bin_width, gc_pmc, 'tracking')
 playback_pmc = get_psth(data_pmc, features, t_pre, t_post, bin_width, gc_pmc, 'playback')
@@ -52,7 +52,28 @@ tracking_aone = get_psth(data_aone, features, t_pre, t_post, bin_width, gc_aone,
 playback_aone = get_psth(data_aone, features, t_pre, t_post, bin_width, gc_aone, 'playback')
 
 # pour récupérer les psth alignés sur les mock changements de fréquence (donc un uniquement en playback, c'est un changement de fréquence qui n'a pas eu lieu acoustiquement mais qui aurait dû avoir eu lieu si on couplait encore le mouvement à au son)
+
 playback_mock_pmc = get_mock_psth(data_pmc, features, t_pre, t_post, bin_width, gc_pmc, 'playback')
 playback_mock_aone = get_mock_psth(data_aone, features, t_pre, t_post, bin_width, gc_aone, 'playback')
+
+
+# pour afficher les psth par channels, et en les organisant tels que sur l'électrode :
+
+fig, axes = plt.subplots(4, 8, figsize=(16, 8))
+fig.suptitle('tracking vs playback vs playback mock PMC', y=1.02)
+plt.subplots_adjust() 
+num_plots, num_rows, num_columns = get_better_plot_geometry(gc_pmc)
+psth_bins = np.arange(-t_pre, t_post, bin_width)
+for n, cluster in enumerate(gc_pmc):
+    if n < num_plots: 
+        row, col = get_plot_coords(cluster)
+        axes[row, col].plot(psth_bins, np.nanmean(playback_mock_pmc[n], axis=0), c = 'blue')
+        axes[row, col].plot(psth_bins, np.nanmean(playback_pmc[n], axis=0), c = 'black')
+        axes[row, col].plot(psth_bins, np.nanmean(tracking_pmc[n], axis=0), c = 'red')
+        axes[row, col].axvline(0, c = 'grey', linestyle='--')
+        axes[row, col].set_title(f'Cluster {cluster}')
+        axes[row, col].spines['top'].set_visible(False)
+        axes[row, col].spines['right'].set_visible(False)
+
 
 
